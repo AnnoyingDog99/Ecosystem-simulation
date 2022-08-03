f4ca7fa5d87097a3f8cfb10c8fbce3aa42e847bd
AnnoyingDog <jan.vlasman99@gmail.com>
2022-08-03 18:35:57 +0200

    mockup to ui

---
+++ Assets/UI/SceneNavigationController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
