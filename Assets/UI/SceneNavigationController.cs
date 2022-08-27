1cf88cc9846a4bea2c8301614dd6dd32778ab795
AnnoyingDog <jan.vlasman99@gmail.com>
2022-08-27 21:00:30 +0200

    SceneNavigation Prototype

--- Assets/UI/SceneNavigationController.cs
+++ Assets/UI/SceneNavigationController.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
+using UnityEditor.SceneTemplate;
+
+
 public class SceneNavigationController : MonoBehaviour
 {
+    public Button createNewWorld;
+    public GroupBox scenes;
+    public VisualTreeAsset sceneUITemplate;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        scenes = root.Q<GroupBox>("Scenes");
+        scenes.Clear();
+
+        createNewWorld = root.Q<Button>("Create");
+
+        createNewWorld.clicked += CreateNewWorldPressed;
+    }
+
+    void CreateNewWorldPressed()
+    {
+        sceneUITemplate.CloneTree(scenes);
+        //https://docs.unity3d.com/Manual/scenes-working-with.html
     }
 }
